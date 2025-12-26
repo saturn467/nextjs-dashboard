edb01fe9d9bcc90e9675f720bf248116d9fcc9c4
Kamil <kmiah0614@gmail.com>
2025-12-26 23:11:08 +0000

    Fix font subset casing

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,6 +3,6 @@ import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
 export const lusitana = Lusitana({
-    subsets: ['Latin'],
+    subsets: ['latin'],
     weight: ['400', '700'],
 })
